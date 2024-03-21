637df0088201e04726c1363760956100aa86b4c7
Mareenraj <robinmareen@gmail.com>
2024-03-21 10:04:03 +0530

    StudentServiceImplTest updated.

--- student/src/test/java/org/mareenraj/student/service/impl/StudentServiceImplTest.java
+++ student/src/test/java/org/mareenraj/student/service/impl/StudentServiceImplTest.java
@@ -11,7 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,47 +41,52 @@ class StudentServiceImplTest {
         studentDto = new StudentDto(1L, "John", "Doe", 25, "XNpZv@example.com", "New York", "Male", "grade 10", "1234567890", 1L);
 
         when(studentConverter.covertToStudent(studentDto)).thenReturn(student);
-        when(studentRepository.save(student)).thenReturn(student);
 
         studentService.saveStudent(studentDto);
 
         verify(studentRepository, times(1)).save(student);
+        verify(studentConverter, times(1)).covertToStudent(studentDto);
     }
 
     @Test
     void findAllStudents() {
         student = new Student(1L, "John", "Doe", 25, "XNpZv@example.com", "New York", "Male", "grade 10", "1234567890", 1L);
         studentDto = new StudentDto(1L, "John", "Doe", 25, "XNpZv@example.com", "New York", "Male", "grade 10", "1234567890", 1L);
-        List<Student> students = Arrays.asList(student);
-        List<StudentDto> studentDtos = Arrays.asList(studentDto);
+        List<Student> students = new ArrayList<>();
+        students.add(student);
+        List<StudentDto> expectedStudentDtos = new ArrayList<>();
+        expectedStudentDtos.add(studentDto);
 
         when(studentRepository.findAll()).thenReturn(students);
         when(studentConverter.covertToStudentDto(student)).thenReturn(studentDto);
 
-        List<StudentDto> studentDtoList = studentService.findAllStudents();
+        List<StudentDto> result = studentService.findAllStudents();
 
         verify(studentRepository, times(1)).findAll();
         verify(studentConverter, times(1)).covertToStudentDto(student);
-        assertEquals(studentDtoList, studentDtos);
+        assertEquals(result, expectedStudentDtos);
     }
 
     @Test
     void findStudentsBySchoolId() {
+        Long schoolId = 1L;
         student = new Student(1L, "John", "Doe", 25, "XNpZv@example.com", "New York", "Male", "grade 10", "1234567890", 1L);
         studentDto = new StudentDto(1L, "John", "Doe", 25, "XNpZv@example.com", "New York", "Male", "grade 10", "1234567890", 1L);
-        List<Student> students = Arrays.asList(student);
-        List<StudentDto> studentDtos = Arrays.asList(studentDto);
-        Long schoolId = 1L;
+        List<Student> students = new ArrayList<>();
+        students.add(student);
+        List<StudentDto> expectedStudentDtos = new ArrayList<>();
+        expectedStudentDtos.add(studentDto);
+
 
         when(studentRepository.findStudentBySchoolId(schoolId)).thenReturn(students);
         when(studentConverter.covertToStudentDto(student)).thenReturn(studentDto);
 
-        List<StudentDto> studentDtoList = studentService.findStudentsBySchoolId(schoolId);
+        List<StudentDto> result = studentService.findStudentsBySchoolId(schoolId);
 
         verify(studentRepository, times(1)).findStudentBySchoolId(schoolId);
         verify(studentConverter, times(1)).covertToStudentDto(student);
-        assertEquals(studentDtoList.size(), 1);
-        assertEquals(studentDtos, studentDtoList);
+        assertEquals(result.size(), 1);
+        assertEquals(result, expectedStudentDtos);
     }
 
     @Test
@@ -107,25 +112,25 @@ class StudentServiceImplTest {
 
     @Test
     void getStudentById() {
+        Long id = 1L;
         student = new Student(1L, "John", "Doe", 25, "XNpZv@example.com", "New York", "Male", "grade 10", "1234567890", 1L);
         studentDto = new StudentDto(1L, "John", "Doe", 25, "XNpZv@example.com", "New York", "Male", "grade 10", "1234567890", 1L);
-        Long id = 1L;
-        Optional<Student> studentOptional = Optional.of(student);
 
-        when(studentRepository.findById(id)).thenReturn(studentOptional);
-        when(studentConverter.covertToStudentDto(studentOptional.get())).thenReturn(studentDto);
+        when(studentRepository.findById(id)).thenReturn(Optional.of(student));
+        when(studentConverter.covertToStudentDto(student)).thenReturn(studentDto);
 
         StudentDto result = studentService.getStudentById(id);
 
         assertEquals(studentDto, result);
+        verify(studentConverter, times(1)).covertToStudentDto(student);
+        verify(studentRepository, times(1)).findById(id);
     }
 
     @Test
     void ifStudentIdNotFound() {
         Long id = 1L;
-        Optional<Student> studentOptional = Optional.empty();
 
-        when(studentRepository.findById(id)).thenReturn(studentOptional);
+        when(studentRepository.findById(id)).thenReturn(Optional.empty());
 
         Throwable exception = assertThrows(StudentNotFoundException.class, () -> studentService.getStudentById(id));
         assertEquals("Student not found with id: " + id, exception.getMessage());
@@ -137,9 +142,9 @@ class StudentServiceImplTest {
         Long id = 1L;
         StudentDto updatedStudentDto = new StudentDto(1L, "Jane", "Smith", 30, "janesmith@example.com", "California", "Female", "grade 11", "9876543210", 2L);
         Student student = new Student(1L, "John", "Doe", 25, "johndoe@example.com", "New York", "Male", "grade 10", "1234567890", 1L);
-        Optional<Student> optionalStudent = Optional.of(student);
 
-        when(studentRepository.findById(id)).thenReturn(optionalStudent);
+
+        when(studentRepository.findById(id)).thenReturn(Optional.of(student));
         when(studentRepository.save(any(Student.class))).thenReturn(student);
         when(studentConverter.covertToStudentDto(any(Student.class))).thenReturn(updatedStudentDto);
 
@@ -150,5 +155,16 @@ class StudentServiceImplTest {
         assertEquals(updatedStudentDto, result);
         verify(studentRepository, times(1)).findById(id);
         verify(studentRepository, times(1)).save(any(Student.class));
+        verify(studentConverter, times(1)).covertToStudentDto(student);
+    }
+
+    @Test
+    void updateStudentByIdIfStudentNotFound() {
+        Long id = 1L;
+        StudentDto studentDto = new StudentDto(1L, "Jane", "Smith", 30, "janesmith@example.com", "California", "Female", "grade 11", "9876543210", 2L);
+
+        when(studentRepository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(StudentNotFoundException.class, () -> studentService.updateStudentById(id, studentDto));
     }
 }
